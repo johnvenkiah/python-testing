0db8ee7f093632965a75f4773f7c95e34bfa085d
johnvenkiah <contact@johnvenkiah.com>
2021-10-13 17:31:49 +0000

    refactor code after passing tests

--- test_evens.py
+++ test_evens.py
@@ -7,7 +7,6 @@ class TestEvens(unittest.TestCase):
     def test_throws_error_if_value_passed_in_is_not_list(self):
         self.assertRaises(TypeError, even_of_evens, 4)
 
-
     def test_values_in_list(self):
         self.assertEqual(even_of_evens([]), False)
         self.assertEqual(even_of_evens([2, 4]), True)
