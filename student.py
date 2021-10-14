60a6819326a7440237572dd6e6411f4a8eea11bc
johnvenkiah <contact@johnvenkiah.com>
2021-10-14 09:43:18 +0000

    tests incl apply extension passing

--- student.py
+++ student.py
@@ -1,27 +1,33 @@
+""" A Student class as base for method testing """
 from datetime import date, timedelta
 
 
 class Student:
     """A Student class as base for method testing"""
 
-    def __init__(self, first_name, last_name, extension):
+    def __init__(self, first_name, last_name):
         self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
-        self.extension = extension
 
     @property
     def full_name(self):
+        """Returns full name of student"""
         return f"{self._first_name} {self._last_name}"
 
     @property
     def email(self):
-        return f"{(self._first_name).lower()}.{(self._last_name).lower()}@email.com"
-
-    def apply_extension(self, days):
-        self.end_date = self.end_date + timedelta(days=days)
+        """Returns email of student"""
+        firstname = self._first_name.lower()
+        lastname = self._last_name.lower()
+        return f"{firstname}.{lastname}@email.com"
 
     def alert_santa(self):
+        """Turns true if student has been naughty"""
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        """Applies an extension prolonging the end date"""
+        self.end_date = self.end_date + timedelta(days=days)
--- test_student.py
+++ test_student.py
@@ -1,6 +1,7 @@
+""" A Student class as base for method testing """
 import unittest
-from student import Student
 from datetime import timedelta
+from student import Student
 
 
 class TestStudent(unittest.TestCase):
@@ -23,34 +24,44 @@ class TestStudent(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        """Class settings for all tests within the class"""
         print('setUpClass')
+
+    def setUp(self):
+        """Settings to be set up for each method"""
+        print('setUp')
+        self.student = Student('John', 'Doe')
 
     @classmethod
     def tearDownClass(cls):
+        """tearDown after all tests within the class"""
         print('tearDownClass')
 
-    def setUp(self):
-        print('setUp')
-        self.student = Student('John', 'Doe', timedelta(days=50))
-
     def tearDown(self):
+        """Settings to be torn down for each method"""
         print('tearDown')
 
     def test_full_name(self):
+        """Test if function full_name returns full name"""
         print('test_full_name')
         self.assertEqual(self.student.full_name, 'John Doe')
 
     def test_email(self):
+        """Test if function email returns email"""
         print('test_email')
         self.assertEqual(self.student.email, 'john.doe@email.com')
 
-    def test_apply_extension(self, days):
+    def test_apply_extension(self):
+        """Test if function apply_extension works"""
         print('test_apply_extension')
         old_end_date = self.student.end_date
         self.student.apply_extension(5)
-        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+        self.assertEqual(
+            self.student.end_date, old_end_date + timedelta(days=5)
+        )
 
     def test_alert_santa(self):
+        """Test if function alert_santa works"""
         print('test_alert_santa')
         self.student.alert_santa()
         self.assertTrue(self.student.naughty_list)
